540585a8d762688f63ff0bb19a574ff8d303413a
musa11971 <mussesemou99@gmail.com>
2023-04-01 17:14:24 +0200

    Added German and French translations

---
+++ resources/lang/de/translations.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'translation-navigation-label' => 'Übersetzungsmanager',
+    'translation-label' => 'Übersetzung|Übersetzungen',
+    'group' => 'Übersetzungsgruppe',
+    'key' => 'Übersetzungsschlüssel',
+    'preview-in-your-lang' => 'Vorschau in Ihrer aktuellen Sprache (:lang)',
+    'synchronize' => 'Synchronisieren',
+    'synchronization-success' => ':count Übersetzungen wurden synchronisiert!',
+    'synchronization-deleted' => ':count ungenutzte Übersetzungen wurden gelöscht',
+    'preview' => 'Vorschau',
+    'preview-description' => 'Dies ist eine Vorschau in der aktuellen ausgewählten Sprache (:lang)',
+    'add-translation-button' => 'Neue Übersetzung hinzufügen',
+    'translation-language' => 'Sprache',
+    'translation-text' => 'Übersetzter Text',
+    'filter-not-translated' => 'Nicht übersetzt in',
+];
---
+++ resources/lang/fr/translations.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'translation-navigation-label' => 'Gestionnaire de traductions',
+    'translation-label' => 'Traduction|Traductions',
+    'group' => 'Groupe de traduction',
+    'key' => 'Clé de traduction',
+    'preview-in-your-lang' => 'Aperçu dans votre langue actuelle (:lang)',
+    'synchronize' => 'Synchroniser',
+    'synchronization-success' => ':count traductions ont été synchronisées !',
+    'synchronization-deleted' => ':count traductions inutilisées ont été supprimées',
+    'preview' => 'Aperçu',
+    'preview-description' => 'Ceci est un aperçu dans la langue actuellement sélectionnée (:lang)',
+    'add-translation-button' => 'Ajouter une nouvelle traduction',
+    'translation-language' => 'Langue',
+    'translation-text' => 'Texte traduit',
+    'filter-not-translated' => 'Non traduit en',
+];
