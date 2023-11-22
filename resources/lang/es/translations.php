78d624d83f5681314db6e33c71df692fb0902f8e
pardalsalcap <dani.casasnovas@gmail.com>
2023-11-22 14:34:47 +0100

    Added translations for Spanish and Catalan
    Added translations for Spanish (Spain es_ES) and Catalan (Catalan from Spain es_CA)

---
+++ resources/lang/es/translations.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'translation-navigation-label' => 'Gestor de traducciones',
+    'translation-label' => 'Traducción|Traducciones',
+    'group' => 'Grupo de traducciones',
+    'key' => 'Clave de traducción',
+    'preview-in-your-lang' => 'Previsualización en tu idioma (:lang)',
+    'synchronize' => 'Sincronizar',
+    'synchronization-success' => 'Se han sincronizado :count traducciones!',
+    'synchronization-deleted' => 'Se han eliminado :count traducciones sin usar',
+    'preview' => 'Previsualizar',
+    'preview-description' => 'Es es un ejemplo en tu idioma (:lang)',
+    'add-translation-button' => 'Añadir traducción',
+    'translation-language' => 'Idioma',
+    'translation-text' => 'Texto traducido',
+    'filter-not-translated' => 'No traducido al ',
+    'quick-translate' => 'Traducción rápida',
+    'quick-translate-select-locale' => 'Selecciona el idioma',
+    'quick-translate-translation-number' => ':total traducciones pendientes',
+    'quick-translate-skip' => 'Saltar esta traducción',
+    'quick-translate-enter' => 'Introducir la traducción en ":lang":',
+    'quick-translate-save-and-continue' => 'Guardar y continuar',
+    'quick-translate-nothing' => 'No queda nada por traducir!',
+];
