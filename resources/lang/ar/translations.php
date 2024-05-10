617d93d604ae4c36b92b2afccfa7df6db209f02c
musta20 <must.g20@gmail.com>
2024-05-11 02:29:19 +0300

    add arabic label translation folder : ar

---
+++ resources/lang/ar/translations.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'translation-navigation-label' => 'مدير الترجمة',
+    'translation-label' => 'ترجمة|ترجمات',
+    'group' => 'مجموعة الترجمة',
+    'key' => 'مفتاح الترجمة',
+    'preview-in-your-lang' => 'المعاينة بلغتك الحالية (:lang)',
+    'synchronize' => 'تزامن',
+    'synchronization-success' => 'متزامنة: عدد الترجمات!',
+    'synchronization-deleted' => 'محذوفة: عدد الترجمات غير المستخدمة',
+    'preview' => 'Preview',
+    'preview-description' => 'هذا مثال باللغة المحددة حاليًا (:lang)',
+    'add-translation-button' => 'إضافة ترجمة جديدة',
+    'translation-language' => 'لغة',
+    'translation-text' => 'نص مترجم',
+    'filter-not-translated' => 'لم تترجم إلى',
+    'quick-translate' => 'ترجمة سريعة',
+    'quick-translate-select-locale' => 'اختر اللغة',
+    'quick-translate-translation-number' => ': إجمالي الترجمات المتبقية للترجمة',
+    'quick-translate-skip' => 'تخطي هذه الترجمة',
+    'quick-translate-enter' => 'أدخل الترجمة باللغة ":lang":',
+    'quick-translate-save-and-continue' => 'حفظ والمتابعة',
+    'quick-translate-nothing' => 'لم يبق شيء للترجمة!',
+];
