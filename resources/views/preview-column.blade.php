e9e39b9a35296944480eb172a77fe07f15594d41
Jehizkia <Jehizkia@users.noreply.github.com>
2024-06-07 14:41:37 +0200

    Added option to preview translation on hover on

--- resources/views/preview-column.blade.php
+++ resources/views/preview-column.blade.php
@@ -1,3 +1,16 @@
-<div>
-    {{ $this->getTranslationPreview($getRecord(), 50) }}
+<div x-data>
+    <template x-ref="template">
+        @foreach($getRecord()->text as $key => $translation)
+            <div class="block"><span class="font-bold">{{ $key }}</span> - {{ $translation }} </div>
+        @endforeach
+
+    </template>
+
+    <button x-tooltip="{
+        content: () => $refs.template.innerHTML,
+        allowHTML: true,
+        appendTo: $root,
+    }">
+        {{ $this->getTranslationPreview($getRecord(), 50) }}
+    </button>
 </div>
